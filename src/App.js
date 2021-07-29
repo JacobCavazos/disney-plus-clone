735e5e1b5a5b329127c81b8b4cb9c378e33187e2
Jacob Cavazos <cavazos.jacob.r@heb.com>
2021-07-29 08:40:54 -0500

    other components

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import Login from './components/Login';
-import Header from './components/Header';
-import './App.css';
-
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Login from "./components/Login";
+import Header from "./components/Header";
+import "./App.css";
+import Home from "./components/Home";
+import Detail from "./components/Detail";
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <Header/>
-      <Switch>
-          <Route exact path='/'>
+        <Header />
+        <Switch>
+          <Route exact path="/">
             <Login />
           </Route>
-      </Switch>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
+          </Route>
+        </Switch>
       </Router>
     </div>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,26 +1,26 @@
-import { useEffect } from 'react';
-import styled from 'styled-components';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom';
-import { auth, provider } from '../firebase';
+import { useEffect } from "react";
+import styled from "styled-components";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { auth, provider } from "../firebase";
 import {
   selectUserName,
   selectUserPhoto,
   setUserLoginDetails,
   setSignOutState,
-} from '../features/user/userSlice';
+} from "../features/user/userSlice";
 
-const Header = props => {
+const Header = (props) => {
   const dispatch = useDispatch();
   const history = useHistory();
   const userName = useSelector(selectUserName);
   const userPhoto = useSelector(selectUserPhoto);
 
   useEffect(() => {
-    auth.onAuthStateChanged(async user => {
+    auth.onAuthStateChanged(async (user) => {
       if (user) {
         setUser(user);
-        history.push('/home');
+        history.push("/home");
       }
     });
   }, [userName]);
@@ -29,10 +29,10 @@ const Header = props => {
     if (!userName) {
       auth
         .signInWithPopup(provider)
-        .then(result => {
+        .then((result) => {
           setUser(result.user);
         })
-        .catch(error => {
+        .catch((error) => {
           alert(error.message);
         });
     } else if (userName) {
@@ -40,28 +40,29 @@ const Header = props => {
         .signOut()
         .then(() => {
           dispatch(setSignOutState());
-          history.push('/');
+          history.push("/");
         })
-        .catch(err => alert(err.message));
+        .catch((err) => alert(err.message));
     }
   };
 
-  const setUser = user => {
+  const setUser = (user) => {
     dispatch(
       setUserLoginDetails({
         name: user.displayName,
         email: user.email,
         photo: user.photoURL,
-      }),
+      })
     );
   };
 
-    return (
-      <Nav>
-        <Logo>
-          <img src='/images/logo.svg' alt='' />
-        </Logo>
-       {!userName ? (
+  return (
+    <Nav>
+      <Logo>
+        <img src="/images/logo.svg" alt="Disney+" />
+      </Logo>
+
+      {!userName ? (
         <Login onClick={handleAuth}>Login</Login>
       ) : (
         <>
@@ -100,57 +101,62 @@ const Header = props => {
         </>
       )}
     </Nav>
-    );
+  );
 };
 
 const Nav = styled.nav`
-position: fixed;
-top: 0;
-left: 0;
-right: 0;
-height: 70px;
-background-color: #090b13;
-display: flex;
-justify-content: space-between;
-align-items: center;
-padding: 0 36px;
-letter-spacing: 16px;
-z-index: 3;
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 70px;
+  background-color: #090b13;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 36px;
+  letter-spacing: 16px;
+  z-index: 3;
 `;
 
 const Logo = styled.a`
-padding: 0;
-width: 80px;
-margin-top: 4px;
-font-size: 0;
-display: inline-block;
-img {
-  display: block;
-  width: 100%;
-}
+  padding: 0;
+  width: 80px;
+  margin-top: 4px;
+  max-height: 70px;
+  font-size: 0;
+  display: inline-block;
+
+  img {
+    display: block;
+    width: 100%;
+  }
 `;
 
 const NavMenu = styled.div`
-align-items: center;
-display: flex;
-flex-flow: row nowrap;
-height: 100px;
-margin: 0;
-padding: 0;
-position: relative;
-margin-right: auto;
-margin-left: 25px;
+  align-items: center;
+  display: flex;
+  flex-flow: row nowrap;
+  height: 100%;
+  justify-content: flex-end;
+  margin: 0px;
+  padding: 0px;
+  position: relative;
+  margin-right: auto;
+  margin-left: 25px;
 
-a {
+  a {
     display: flex;
     align-items: center;
     padding: 0 12px;
+
     img {
       height: 20px;
       min-width: 20px;
       width: 20px;
       z-index: auto;
     }
+
     span {
       color: rgb(249, 249, 249);
       font-size: 13px;
@@ -159,6 +165,7 @@ a {
       padding: 2px 0px;
       white-space: nowrap;
       position: relative;
+
       &:before {
         background-color: rgb(249, 249, 249);
         border-radius: 0px 0px 4px 4px;
@@ -176,6 +183,7 @@ a {
         width: auto;
       }
     }
+
     &:hover {
       span:before {
         transform: scaleX(1);
@@ -185,25 +193,25 @@ a {
     }
   }
 
-@media (max-width: 768px) {
-  display: none;
-}
+  /* @media (max-width: 768px) {
+    display: none;
+  } */
 `;
 
 const Login = styled.a`
-background-color: black;
-padding: 8px 16px;
-text-transform: uppercase;
-letter-spacing: 1.5px;
-border: 1px solid #f9f9f9;
-border-radius: 4px;
-transition: all 0.2s ease 0s;
+  background-color: rgba(0, 0, 0, 0.6);
+  padding: 8px 16px;
+  text-transform: uppercase;
+  letter-spacing: 1.5px;
+  border: 1px solid #f9f9f9;
+  border-radius: 4px;
+  transition: all 0.2s ease 0s;
 
-&:hover {
-  background-color: #f9f9f9;
-  color: #000;
-  border-color: transparent;
-}
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
 `;
 
 const UserImg = styled.img`
@@ -233,11 +241,13 @@ const SignOut = styled.div`
   cursor: pointer;
   align-items: center;
   justify-content: center;
+
   ${UserImg} {
     border-radius: 50%;
     width: 100%;
     height: 100%;
   }
+
   &:hover {
     ${DropDown} {
       opacity: 1;
